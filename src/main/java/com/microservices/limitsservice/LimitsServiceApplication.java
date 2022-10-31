405035c54dbb99b49359422d0d0df89d9ec35b6a
suryajit7 <suryajit7@gmail.com>
2022-10-31 16:20:57 +0530

    adding working code

--- src/main/java/com/microservices/limitsservice/LimitsServiceApplication.java
+++ src/main/java/com/microservices/limitsservice/LimitsServiceApplication.java
@@ -2,6 +2,8 @@ package com.microservices.limitsservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
 
 @SpringBootApplication
 public class LimitsServiceApplication {
